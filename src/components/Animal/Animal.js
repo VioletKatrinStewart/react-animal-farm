78a22a9b83db0a61df3b2d3ff11779a04d65677c
Violet Stewart <vkazoo2@gmail.com>
2021-11-29 15:51:27 -0800

    animals are displayed with names

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import './Animal.css'
 
-export default function Animal() {
-  return <></>
+export default function Animal(props) {
+  return (
+    <>
+      <div className="animal" style={{ left: props.left, top: props.top }}>
+        <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+        <span>{props.name}</span>
+      </div>
+    </>
+  )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import './Main.css'
+import background from '../../background.png'
+import { animals } from '../../data'
+import Animal from '../Animal/Animal'
 
 export default function Main() {
-  return <main></main>
+  return (
+    <main>
+      <img src={background} alt="farmyard" />
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          type={animal.type}
+          name={animal.name}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  )
 }
